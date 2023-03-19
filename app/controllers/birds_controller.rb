e4dea1dc1f4cb1e3f6deab7091a24075f9a45587
Zeddzuruzuranda <raphomelvo@gmail.com>
2023-03-19 17:36:41 +0300

    completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -5,6 +5,10 @@ class BirdsController < ApplicationController
     birds = Bird.all
     render json: birds
   end
+  def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
 
   # GET /birds/:id
   def show
@@ -14,6 +18,11 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  private
+  def bird_params
+    params.permit(:name,:species)
   end
 
 end
